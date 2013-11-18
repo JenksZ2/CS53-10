da33b98c50811e30d1a08744de2d7bebe0b45e20
Tyler Jenkins <Jenks0002@gmail.com>
2013-11-18 16:02:49 -0600

    Update customer.h

--- customer.h
+++ customer.h
@@ -1,1 +1,16 @@
-//NAME:
+//NAME:         HICKS, MICAHEL / JENKINS, TYLER
+//CLASS:        CS 53
+//SECTION:      E
+//DATE:         11-18-2013
+//DESCRIPTION:  
+using namespace std;
+
+#ifndef HEADER.H
+#define HEADER.H
+
+
+
+
+
+
+#endif
